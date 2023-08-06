85cd4b7447f56f21c396a89a30f6acb7c9e80381
ValeryKot <kot.valery@gmail.com>
2023-08-06 16:31:18 +0300

    add nav end posts

---
+++ app/api/content/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+// Dummy data
+const posts = [
+  {
+    title: 'Lorem Ipsum',
+    slug: 'lorem-ipsum',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+  },
+  {
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    slug: 'quia-et',
+    content:
+      'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    title: 'eum et est occaecati',
+    slug: 'eum-et-est',
+    content:
+      'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit',
+  },
+];
+
+export async function GET() {
+  return NextResponse.json(posts);
+}
